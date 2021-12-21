f0a6f354ba87a5e259bb449286fbc39449a7e065
bajra <bajra.idabagus@karmagroup.com>
2021-12-21 17:10:08 +0800

    update register

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import {  RegisterBg2 } from '../../assets';
-import { Button, Gap, Input } from '../../components';
+import { Button, Gap, Input, Link } from '../../components';
 import './register.scss';
 
 function Register() {
   return (
     <div className="main-page">
-      <div className="left">
-        <img src={ RegisterBg2 } className="bg-registration" alt="register" />
+      <div className="left" style={
+        {
+          backgroundImage: `url(${RegisterBg2})`,
+          backgroundPosition: 'center',
+          backgroundSize: 'cover'
+        }
+      }>
+        {/* <img src={ RegisterBg2 } className="bg-registration" alt="register" /> */}
       </div>
       <div className="right">
-        <div className="form-container">
+        <form className="form-container">
           <h1>Form Register</h1>
           <Input label="" placeholder="Full Name" type="text" id="fullname" required="true" autocomplete="off" />
           <Gap height={15} />
@@ -19,7 +25,9 @@ function Register() {
           <Input label="" placeholder="Password" type="password" id="password" required="true" autocomplete="off" />
           <Gap height={30} />
           <Button title="Register" type="submit" />
-        </div>
+          <Gap height={20} />
+          <Link title="Back to login" />
+        </form>
       </div>
     </div>
   )
