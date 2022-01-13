d71fa14a130f481171c751200afabb7a03dff287
bajra <bajra.idabagus@karmagroup.com>
2022-01-13 17:16:58 +0800

    update layout

--- src/components/molecules/BlogItem/index.js
+++ src/components/molecules/BlogItem/index.js
@@ -3,8 +3,13 @@ import './blogitem.scss'
 
 const BlogItem = () => {
   return (
-    <div>
-      <p>Blog Items</p>
+    <div className="blog-container">
+      <div className="blog-thumb">Thumb</div>
+      <div className="blog-body">
+        <div className="blog-author">Author</div>
+        <div className="blog-title"><h2>Blog Title</h2></div>
+        <div className="blog-desc"><p>Blog excerpt</p></div>
+      </div>
     </div>
   )
 }
