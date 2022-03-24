ffebd45f8713aa7bb34bbbb17c60573cb80163ae
bajra <bajra.idabagus@karmagroup.com>
2022-03-24 17:01:07 +0800

    update home axios

--- src/components/molecules/BlogItem/index.js
+++ src/components/molecules/BlogItem/index.js
@@ -2,16 +2,16 @@ import React from 'react'
 import './blogitem.scss'
 import { Link } from 'react-router-dom'
 
-const BlogItem = ({ thumbnail, author, date, title, content }) => {
+const BlogItem = (props) => {
   return (
     <div className="blog-container">
       <div className="blog-thumb">
-        <Link to="/detail-blog"><img src={thumbnail} alt="Thumbnail" /></Link>
+        <Link to="/detail-blog"><img src={props.image} alt="Thumbnail" /></Link>
       </div>
       <div className="blog-body">
-        <div className="blog-title"><Link to="/detail-blog"><h2>{ title }</h2></Link></div>
-        <div className="blog-author">Author: {author} - Date Posted: {date}</div>
-        <div className="blog-content"><p>{ content }</p></div>
+        <div className="blog-title"><Link to="/detail-blog"><h2>{ props.title }</h2></Link></div>
+        <div className="blog-author">Author: {props.author} - Date Posted: {props.date}</div>
+        <div className="blog-content"><p>{ props.body }</p></div>
         <Link to="/detail-blog" className="read-more">Read more...</Link>
       </div>
     </div>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BlogItem, Button, Gap } from '../../components'
 import './home.scss';
 import { DummyBlog } from '../../assets';
 import { useNavigate } from 'react-router-dom';
+import Axios from 'axios';
 
 function Home() {
+  const [dataBlog, setDataBlog] = useState([]);
+  const url = 'http://localhost:4000/v1/blog/post';
+  useEffect(() => {
+    Axios.get(url)
+      .then(result => {
+        console.log('Data API: ', result.data.data);
+        const responseAPI = result.data;
+
+        setDataBlog(responseAPI.data);
+      })
+      .catch(err => {
+        console.log('Error: ', err);
+      })
+  }, []);
+
   const navigate = useNavigate();
+  
 
   return (
     <div className="home-page-container">
@@ -13,9 +30,12 @@ function Home() {
         <Button title="Create Blog" onClick={() => navigate('/create-blog')} />
       </div>
       <div className="blog-wrapper">
-        <BlogItem thumbnail={ DummyBlog } title="Blog 1" author="Bajra" date="01 Jan 2022" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor quis leo in facilisis. In posuere enim id eros mattis, eget convallis erat tincidunt. Donec id justo at est aliquet vestibulum. Praesent ut bibendum sem. Nam pharetra, mi vel tempor ullamcorper, leo lacus facilisis magna, quis posuere justo magna in neque."/>
+        {dataBlog.map(blog => {
+          return <BlogItem key={blog._id} image={`${url}${blog.image}`} title={blog.title} author={blog.author.name} date={blog.createdAt} body={ blog.body } />
+        }) }
+        {/* <BlogItem thumbnail={ DummyBlog } title="Blog 1" author="Bajra" date="01 Jan 2022" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor quis leo in facilisis. In posuere enim id eros mattis, eget convallis erat tincidunt. Donec id justo at est aliquet vestibulum. Praesent ut bibendum sem. Nam pharetra, mi vel tempor ullamcorper, leo lacus facilisis magna, quis posuere justo magna in neque."/>
         <BlogItem thumbnail={ DummyBlog } title="Blog 2" author="Bajra" date="01 Jan 2022" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor quis leo in facilisis. In posuere enim id eros mattis, eget convallis erat tincidunt. Donec id justo at est aliquet vestibulum. Praesent ut bibendum sem. Nam pharetra, mi vel tempor ullamcorper, leo lacus facilisis magna, quis posuere justo magna in neque." />
-        <BlogItem thumbnail={ DummyBlog } title="Blog 3" author="Bajra" date="01 Jan 2022" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor quis leo in facilisis. In posuere enim id eros mattis, eget convallis erat tincidunt. Donec id justo at est aliquet vestibulum. Praesent ut bibendum sem. Nam pharetra, mi vel tempor ullamcorper, leo lacus facilisis magna, quis posuere justo magna in neque." />
+        <BlogItem thumbnail={ DummyBlog } title="Blog 3" author="Bajra" date="01 Jan 2022" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor quis leo in facilisis. In posuere enim id eros mattis, eget convallis erat tincidunt. Donec id justo at est aliquet vestibulum. Praesent ut bibendum sem. Nam pharetra, mi vel tempor ullamcorper, leo lacus facilisis magna, quis posuere justo magna in neque." /> */}
       </div>
       <div className="pagination-wrapper">
         <Button title="Prev" />
